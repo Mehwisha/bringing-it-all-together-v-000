05e6c1b432fa9952598ee78de5c711b91737433a
Mehwish Aziz <mehwisha@msn.com>
2018-08-07 03:55:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,17 @@ def initialize(attributes)
   attributes.each {|key, value| self.send(("#{key}="), value)}
       self.id ||= nil
     end
+def self.create_table
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
 
+      DB[:conn].execute(sql)
+end
 
 
 
